49d5525b53d83cde75626d40035c211fbf766d84
Stephen YUEN <sy1223@gmail.com>
2019-10-24 22:25:04 +0800

    Updated main activity test

--- app/src/androidTest/java/com/example/codetest/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/codetest/MainActivityTest.kt
@@ -5,18 +5,12 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.intent.Intents
-import android.support.test.espresso.intent.Intents.intended
-import android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName
-import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import android.support.test.espresso.matcher.RootMatchers.isDialog
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.example.codetest.model.DummyData
 import org.hamcrest.Matchers.*
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -27,11 +21,6 @@ class MainActivityTest {
     @Rule
     @JvmField
     var activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
-
-    @Before
-    fun setUp() {
-        Intents.init()
-    }
 
     @Test
     fun testEmptyFromAccount() {
@@ -106,18 +95,9 @@ class MainActivityTest {
         onView(withId(R.id.etAmount)).perform(typeText("16.7"))
         onView(withId(R.id.btnConfirm)).perform(click())
 
-        Thread.sleep(2000) // Wait for 2 seconds
-
-        intended(hasComponent(hasClassName(TransferSuccessActivity::class.java.name)))
-
         onView(withId(R.id.tvReferenceNumber)).check(matches(withText(startsWith("REF"))))
         onView(withId(R.id.tvFromAccountNumber)).check(matches(withText(accounts[1])))
         onView(withId(R.id.tvToAccountNumber)).check(matches(withText(accounts[2])))
         onView(withId(R.id.tvAmount)).check(matches(withText("16.7")))
-    }
-
-    @After
-    fun tearDown() {
-        Intents.release()
     }
 }
